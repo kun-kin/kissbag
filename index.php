60adaec70c5c91590a2c237752d807be0c32b61d
kun-kin <57570442+kun-kin@users.noreply.github.com>
2021-06-08 20:54:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,161 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name = "viewport" content = "width=device-width, initial-scale = 1">
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+    <link rel="stylesheet" type="text/css" href="css/content.css" />
+    <link rel="stylesheet" type="text/css" href="css/footer.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style type="text/css">
+         body{
+         	width: 1447px;
+			height: 3171px;
+			background: #FBFCFD;
+         }
+         header{
+         	   background-image: url("image/imageheader.png");
+			   height: 623px;
+			   
+         }
+        
+    </style>
+</head>
+<body>
+	<div id="all">
+		<?php include "header.php" ?> 
+		
+		<content id="content">
+			<div>
+				<div id="content1">
+				<div>
+					<div id="content1-1">
+						<img src="image/image 11.png">
+					</div>
+					<div id="content1-2">
+						<img src="image/image 12.png">
+					</div>
+					<div id="content1-3">
+						<img src="image/image 13.png">
+					</div>
+					<div id="content1-4">
+						<img src="image/image 14.png">
+					</div>
+					<div id="content1-5">
+						<img src="image/image 15.png">
+					</div>
+					<div id="content1-6">
+						<img src="image/image 16.png">
+					</div>
+					<div id="content1-7">
+						<img src="image/image 17.png">
+					</div>
+				</div>
+			</div>
+			<div id="content2">
+				<div>
+					<p id="p7">CÁC TÍNH NĂNG</p>
+					<div id="bo"></div>
+					<div id="content2-1">
+						<div id="content2-1-1">				
+							<img src="image/image1.png" id="img1">
+							<p id="p8">Đăng tuyển miễn phí</p>
+							<p id="p11">Nếu bạn là nhà tuyển dụng thì nên biết: Timviec365.com là công cụ đăng tin tuyển dụng dễ dàng và hoàn toàn miễn phí</p>
+		                </div>
+						<div id="content2-1-2">
+							<img src="image/image2.png" id="img2">
+							<p id="p9">Kênh việc làm</p>
+							<p id="p12">Timviec365.com đang nỗ lực để trở thành kênh việc làm hàng đầu tại Việt Nam, giúp kết nối Ứng viên và Nhà tuyển dụng nhanh và hiệu quả nhất</p>
+						</div>
+						<div id="content2-1-3">
+							<img src="image/image3.png" id="img3">
+							<p id="p10">Thống kê số liệu</p>
+							<p id="p13">Với số liệu sẽ được cập nhật liên tục. Giúp cho người dùng dễ dàng theo dõi việc đã đăng và lượng người ứng tuyển.</p>
+						</div>	
+					</div>
+				</div>
+			</div>
+			<div id="content3">
+				<div>
+					<div id="content3-1">
+						<div id="content3-1-1">
+							<p style="color: #3797DD" style="font-weight: bold;">1,686,780</p>
+							<p>Tin tuyển dụng</p>
+						</div>
+						<div id="content3-1-2">
+							<p style="color: #3797DD" style="font-weight: bold;">24,687</p>
+							<p>Nhà tuyển dụng</p>
+						</div>
+						<div id="content3-1-3">
+							<p style="color: #3797DD" style="font-weight: bold;">615,451</p>
+						    <p>CV tạo mới</p>
+						</div>
+					</div>
+					<p id="p14">Timviec365.com là công cụ tìm việc làm, kết nối tuyển dụng hiệu quả, có ưu điểm tạo CV chuyên nghiệp miễn phí. Giúp nhà tuyển dụng đăng tin miễn phí, tìm ứng viên phù hợp.</p>
+					<div id="content3-2">
+						<p id="p15">Đăng ký ngay</p>
+					</div>
+					<div><img src="image/image4.png" id="img4"></div>
+				</div>
+			</div>
+			<div id="content4">
+				<div style="width: 100%;height: 100%">
+					<div id="imgct4">
+						<div><img src="image/content4.png" id="imgct41"></div>
+						<div>
+							<p id="ct4">Hệ thống chủ động tìm & gợi ý ứng viên</p>
+							<p id="ct41">Sau khi bạn đăng tin tuyển dụng, hệ thống sẽ gợi ý ứng viên phù hợp nhất cho bạn, tiết kiệm thời gian, tăng hiệu quả tuyển dụng.</p>
+							<div id="btn">
+								<p align="center" style="color: #FFFFFF">TÌM ỨNG VIÊN</p>
+							</div>
+						</div>
+				    </div>
+				</div>
+			</div>
+			<div id="content5">
+				<div>
+					<p id="ct5">Quản lý thông tin dễ dàng</p>
+					<p id="ct51">Nhà tuyển dụng dễ dàng quản lý tin đăng tuyển, quản lý ứng viên và tương tác trực tiếp với ứng viên</p>
+					<div id="imgct5">
+						<img src="image/imgct5.png" >
+					</div>
+				</div>
+			</div>
+			<!-- <div id="content6" align="center">
+				<div>
+					<div id="ct61">
+						<p id="ct611">NHÀ TUYỂN DỤNG NÓI GÌ?</p>
+						<p id="ct612">Sự hài lòng của Nhà tuyển dụng là sứ mệnh của chúng tôi</p>
+
+					</div>
+					<div id="ct62" >
+						<img src="image/ct61.png">
+						<p align="center">Nguồn ứng viên trên Timviec365.com rất lớn và chất lượng, tôi đã tuyển đủ số lượng ứng viên trong thời gian ngắn</p>
+					</div>
+					<div id="ct63">
+						<img src="image/ct62.png" >
+						<p>Tôi ấn tượng mạnh với tính năng quản lý ứng viên của Timviec365.com. Ứng viên rất tuyệt voeif và chuyên nghiệp</p>
+					</div>
+					<div id="ct64">
+						<img src="image/ct63.png">
+						<p>Timviec365.com giúp tôi tiếp cận nhiều hơn với ứng viên chất lượng. Các mẫu CV cũng rất đẹp và dễ nhìn</p>
+					</div>
+					<div id="ct65">
+						<p><i class="fa fa-hand-paper-o" aria-hidden="true"> Chát trực tuyến</i> </p>
+					</div>
+					<div id="ct66">
+						<p><i class="fa fa-envelope-o" aria-hidden="true"></i>  Timvieclam365.com</p>
+					</div>
+					<div id="ct67">
+						<p><i class="fa fa-volume-control-phone" aria-hidden="true"></i>  085957495</p>
+					</div>
+				</div> -->
+			   </div>
+			</div>
+		</content>
+		
+	   <?php include "footer.php" ?>
+	</div>
+</body>
+</html>
